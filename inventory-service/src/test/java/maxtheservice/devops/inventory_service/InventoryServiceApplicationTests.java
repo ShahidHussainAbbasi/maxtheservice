fae7f238d1b40fee79e7b0cd10df89868470c176
ShahidHussainAbbasi <uncer_sh@yahoo.com>
2025-07-29 07:27:43 +0500

    microservices and component added

---
+++ inventory-service/src/test/java/maxtheservice/devops/inventory_service/InventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package maxtheservice.devops.inventory_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
